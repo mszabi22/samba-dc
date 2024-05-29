be0a86d1816a44a1557f5d9a5048f0ea84f9802b
mszabi22 <majorsza@gmail.com>
2024-05-29 09:00:12 +0200

    Update OU scripts

--- USER-MANAGEMENT/00-ou/create-ou.sh
+++ USER-MANAGEMENT/00-ou/create-ou.sh
@@ -2,11 +2,13 @@
 
 samba-tool ou create OU=Diákok
 samba-tool ou create OU=Dolgozók
-samba-tool ou create OU=Tanárok,OU=Dolgozók
-samba-tool ou create OU=Stuff,OU=Dolgozók
 samba-tool ou create OU=Gazdasági,OU=Dolgozók
-samba-tool ou create OU=Titkárság,OU=Dolgozók
 samba-tool ou create OU=Hallgatók,OU=Dolgozók
+samba-tool ou create OU=Kollégium,OU=Dolgozók
+samba-tool ou create OU=Stuff,OU=Dolgozók
+samba-tool ou create OU=Tanárok,OU=Dolgozók
+samba-tool ou create OU=Technikai,OU=Dolgozók
+samba-tool ou create OU=Titkárság,OU=Dolgozók
 
 echo "Osztály csoportok létrehozása az aktuális évre..."
 for i in `cat osztalyok.csv`; do
