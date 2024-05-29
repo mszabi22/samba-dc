42bce1500550355a16e5ebadb6521c5102f10c42
mszabi22 <majorsza@gmail.com>
2024-05-29 09:01:31 +0200

    Update GROUPS scripts

--- USER-MANAGEMENT/01-group-create/create-groups.sh
+++ USER-MANAGEMENT/01-group-create/create-groups.sh
@@ -7,13 +7,13 @@ for i in `cat osztalyok.csv`; do
 done
 
 echo "Egyéb csoportok létrehozása..."
-samba-tool group add dolgozo
-samba-tool group add tanar
 samba-tool group add diak
+samba-tool group add dolgozo
 samba-tool group add gazdasagi
-samba-tool group add titkarsag
-samba-tool group add stuff
-samba-tool group add kollegium
 samba-tool group add hallgato
+samba-tool group add kollegium
+samba-tool group add stuff
+samba-tool group add tanar
+samba-tool group add titkarsag
 
 samba-tool group list | sort
