268d49794431fa56510d231856fddf06534580cb
mszabi22 <majorsza@gmail.com>
2024-05-29 08:15:37 +0200

    Update USER manager scripts

---
+++ USER-MANAGEMENT/02-user-import/import-diakok.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while IFS=, read -r osztaly username password surname givenname
+do
+#	JELSZO=`pwgen 8 1`
+    JELSZO=$password
+clear
+echo "Diák hozzáadása..."
+    samba-tool user create --must-change-at-next-login --script-path="diak.cmd" --surname="$surname" --given-name="$givenname" --mail-address="$username@diak.crnl.hu" $username $JELSZO
+echo "Diák csoporthoz adása..."
+    samba-tool group addmembers diak $username;
+echo "Diák szervezeti egységhez adása..."
+    samba-tool user move $username "ou=$osztaly,ou=Diákok"
+echo "HOME létrehozása..."
+    mkdir -p /home/$username; chmod -R 777 /home/$username;
+    echo "$group,$username,$JELSZO" >> /home/$USER/new-worker.csv
+done < diakok.csv
