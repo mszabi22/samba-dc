8c5d9bd09eab1c3ade4d839a41a48c8c59fd239e
mszabi22 <majorsza@gmail.com>
2024-05-29 08:58:12 +0200

    Update USERS scripts

---
+++ USER-MANAGEMENT/02-user-import/import-kollegium.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while IFS=, read -r username password surname givenname
+do
+#	JELSZO=`pwgen 8 1`
+    JELSZO=$password
+clear
+echo "Kollégiumi dolgozó hozzáadása..."
+    samba-tool user create --must-change-at-next-login --script-path="kollegium.cmd" --surname="$surname" --given-name="$givenname" --mail-address="$username@crnl.hu" $username $JELSZO
+echo "Kollégiumi dolgozó csoporthoz adása..."
+    samba-tool group addmembers titkarsag $username;
+echo "Kollégiumi dolgozó szervezeti egységhez adása..."
+    samba-tool user move $username "ou=Kollégium,ou=Dolgozók"
+echo "HOME létrehozása..."
+    mkdir -p /home/$username; chmod -R 777 /home/$username;
+    echo "$group,$username,$JELSZO" >> /home/pingvin/new-worker.csv
+done < kollegium.csv
