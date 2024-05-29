ef7c5f2f27028794cb48ebeb5a1e0b4b5f4ea7c9
mszabi22 <majorsza@gmail.com>
2024-05-29 09:12:25 +0200

    Update User deleting

--- USER-MANAGEMENT/03-user-delete/diak-delete.sh
+++ USER-MANAGEMENT/03-user-delete/diak-delete.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
-samba-tool group listmembers 2020-12A > delete-users.csv
-samba-tool group listmembers 2020-12B >> delete-users.csv
-samba-tool group listmembers 2020-12C >> delete-users.csv
-samba-tool group listmembers 2020-12D >> delete-users.csv
-samba-tool group listmembers 2020-12E >> delete-users.csv
+DATUM=`date +%Y%m%d`
+clear
+samba-tool group listmembers 2023-12A > delete-users.csv
+samba-tool group listmembers 2023-12B >> delete-users.csv
+samba-tool group listmembers 2023-12C >> delete-users.csv
+samba-tool group listmembers 2023-12D >> delete-users.csv
+samba-tool group listmembers 2023-12E >> delete-users.csv
 
 
 for i in `cat delete-users.csv`; do
     samba-tool user delete $i
-    rm -rf /share/home/diak/$i
+echo "Mentés készítése..."
+    tar czvf /home/BACKUP/DIAK-$DATUM-$i /home/$i
+echo "$i mappájának törlése..."
+    rm -rf /home/$i
 done
