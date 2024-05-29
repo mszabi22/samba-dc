0df6c56b07437734681ad3d6a8f68649bfa3251a
mszabi22 <majorsza@gmail.com>
2024-05-29 08:40:52 +0200

    Update USERS scripts

--- USER-MANAGEMENT/00-ou/create-ou.sh
+++ USER-MANAGEMENT/00-ou/create-ou.sh
@@ -6,10 +6,11 @@ samba-tool ou create OU=Tanárok,OU=Dolgozók
 samba-tool ou create OU=Stuff,OU=Dolgozók
 samba-tool ou create OU=Gazdasági,OU=Dolgozók
 samba-tool ou create OU=Titkárság,OU=Dolgozók
+samba-tool ou create OU=Hallgatók,OU=Dolgozók
 
 echo "Osztály csoportok létrehozása az aktuális évre..."
 for i in `cat osztalyok.csv`; do
-    samba-tool ou create OU=2024-$i,OU=Diákok
+    samba-tool ou create OU=2023-$i,OU=Diákok
 done
 
 samba-tool ou list | sort
---
+++ USER-MANAGEMENT/02-user-import/import-hallgato.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while IFS=, read -r username password surname givenname
+do
+#	JELSZO=`pwgen 8 1`
+    JELSZO=$password
+clear
+echo "Hallgató hozzáadása..."
+    samba-tool user create --must-change-at-next-login --script-path="tanar.cmd" --surname="$surname" --given-name="$givenname" --mail-address="$username@guest.crnl.hu" $username $JELSZO
+echo "Hallgató csoporthoz adása..."
+    samba-tool group addmembers tanar $username;
+    samba-tool group addmembers hallgato $username;
+echo "Hallgató szervezeti egységhez adása..."
+    samba-tool user move $username "ou=Hallgatók,ou=Dolgozók"
+echo "HOME létrehozása..."
+    mkdir -p /home/$username; chmod -R 777 /home/$username;
+    echo "$group,$username,$JELSZO" >> /home/pingvin/new-worker.csv
+done < gazdasagi.csv
