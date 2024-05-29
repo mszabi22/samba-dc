1757aa775d8d998d39f9a16f8ad8b995861c393d
mszabi22 <majorsza@gmail.com>
2024-05-29 07:49:29 +0200

    Update USER manager scripts

---
+++ USER-MANAGEMENT/02-user-import/import-tanarok.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while IFS=, read -r username password surname givenname
+do
+#	JELSZO=`pwgen 8 1`
+    JELSZO=$password
+clear
+echo "Tanár hozzáadása..."
+    samba-tool user create --must-change-at-next-login --script-path="tanar.cmd" --surname="$surname" --given-name="$givenname" --mail-address="$username@crnl.hu" $username $JELSZO
+echo "Tanár csoporthoz adása..."
+    samba-tool group addmembers tanar $username;
+echo "Tanár szervezeti egységhez adása..."
+    samba-tool user move $username "ou=Tanárok,ou=Dolgozók"
+echo "HOME létrehozása..."
+    mkdir -p /home/$username; chmod -R 777 /home/$username;
+    echo "$group,$username,$JELSZO" >> /home/$USER/new-worker.csv
+done < tanarok.csv
