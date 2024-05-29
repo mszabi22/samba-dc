c452c78c374a9823c71f3ad0e4c1d7517f455c8b
mszabi22 <majorsza@gmail.com>
2024-05-29 08:56:33 +0200

    Update USERS scripts

---
+++ USER-MANAGEMENT/02-user-import/import-titkarsag.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while IFS=, read -r username password surname givenname
+do
+#	JELSZO=`pwgen 8 1`
+    JELSZO=$password
+clear
+echo "Titkársági dolgozó hozzáadása..."
+    samba-tool user create --must-change-at-next-login --script-path="titkarsag.cmd" --surname="$surname" --given-name="$givenname" --mail-address="$username@crnl.hu" $username $JELSZO
+echo "Titkársági dolgozó csoporthoz adása..."
+    samba-tool group addmembers titkarsag $username;
+echo "Titkársági dolgozó szervezeti egységhez adása..."
+    samba-tool user move $username "ou=Titkárság,ou=Dolgozók"
+echo "HOME létrehozása..."
+    mkdir -p /home/$username; chmod -R 777 /home/$username;
+    echo "$group,$username,$JELSZO" >> /home/pingvin/new-worker.csv
+done < titkarsag.csv
