40427965401b3d920619ac5ec82e55741a030a60
Szabi <majorsza@gmail.com>
2024-06-03 09:02:55 +0200

    Add staff.cmd

---
+++ CONFIG/sysvol/staff.cmd
@@ -0,0 +1,8 @@
+net use u: \\zentyal\%USERNAME%
+net use i: \\zentyal\install
+net use s: \\zentyal\diak
+net use t: \\zentyal\tanar
+net use o: \\zentyal\titkarsag
+net use z: \\zentyal\gazdasagi
+
+net time \\zentyal /set /yes
