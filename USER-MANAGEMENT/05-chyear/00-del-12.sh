48ff3aa0fb5ed19f43b73142664f8d63d2150a37
Szabi <majorsza@gmail.com>
2024-06-03 08:25:53 +0200

    Update: deleting protokoll

--- USER-MANAGEMENT/05-chyear/00-del-12.sh
+++ USER-MANAGEMENT/05-chyear/00-del-12.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-OLDYEAR="2020"
+LASTYEAR="2020"
 
-samba-tool group listmembers $OLDYEAR-12A > delete-users.csv
-samba-tool group listmembers $OLDYEAR-12B >> delete-users.csv
-samba-tool group listmembers $OLDYEAR-12C >> delete-users.csv
-samba-tool group listmembers $OLDYEAR-12D >> delete-users.csv
-samba-tool group listmembers $OLDYEAR-12E >> delete-users.csv
+samba-tool group listmembers $LASTYEAR-12A > delete-users.csv
+samba-tool group listmembers $LASTYEAR-12B >> delete-users.csv
+samba-tool group listmembers $LASTYEAR-12C >> delete-users.csv
+samba-tool group listmembers $LASTYEAR-12D >> delete-users.csv
+samba-tool group listmembers $LASTYEAR-12E >> delete-users.csv
 
 for i in `cat delete-users.csv`; do
     samba-tool user delete $i
