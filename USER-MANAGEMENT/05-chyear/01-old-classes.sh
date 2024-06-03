3ed4f1596abeef80baacfbbb2e28e417a6cee4af
Szabi <majorsza@gmail.com>
2024-06-03 08:33:46 +0200

    Update: deleting protokoll

--- USER-MANAGEMENT/05-chyear/01-old-classes.sh
+++ USER-MANAGEMENT/05-chyear/01-old-classes.sh
@@ -8,8 +8,9 @@ YEAR="2024"
 
 echo "Előző évi osztályok exportálása..."
 for j in `cat osztalyok.csv`; do
-    
-    echo "samba-tool group rename $LASTYEAR-$j --samaccountname=$YEAR-$j"
+#    echo "samba-tool group rename $LASTYEAR-$j --samaccountname=$YEAR-$j"
+#echo "$LASTYEAR-$j --> $YEAR-$j"
+echo "$YEAR-$j" | cut -c 5-7
 done
 
 
