40a9b198be36df7dfaaded26aa3042d89c0cf432
Major Szabolcs <majorsza@gmailcom>
2024-06-03 10:35:39 +0200

    Add multiple DNS script

--- DNS/add-multiple-dns-to-zone.sh
+++ DNS/add-multiple-dns-to-zone.sh
@@ -4,7 +4,7 @@ DOMAIN="domain.com"
 
 while IFS=, read -r domain name ip
 do
-echo    samba-tool dns add localhost $domain $name A $ip -U administrator --password=$JELSZO
+    samba-tool dns add localhost $domain $name A $ip -U administrator --password=$JELSZO
 done < dns.csv
 
 samba-tool dns query localhost $DOMAIN @ ALL -U administrator --password=$JELSZO
