4e2e461f574b2dbb076f7e82d509a8a9c2ef1570
Major Szabolcs <majorsza@localhost.localdomain>
2024-06-04 11:05:16 +0200

    Set home premission

--- USER-MANAGEMENT/set-home-permission.sh
+++ USER-MANAGEMENT/set-home-permission.sh
@@ -3,8 +3,8 @@ DOMAIN="domain"
 
 for username in `samba-tool user list`; do
 if [ ! -d /home/$username ]; then
-  mkdir -p /home/$username;
+    mkdir -p /home/$username;
 fi
-echo    chown -R "$username:$DOMAIN\domain users" /home/$username
+    chown -R "$username:$DOMAIN\domain users" /home/$username
 done
 
