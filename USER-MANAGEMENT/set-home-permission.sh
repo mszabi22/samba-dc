b8fa1cd0913144c87f6e818a28c986d5a9ce40c0
Major Szabolcs <majorsza@localhost.localdomain>
2024-06-04 10:04:54 +0200

    Set quota

--- USER-MANAGEMENT/set-home-permission.sh
+++ USER-MANAGEMENT/set-home-permission.sh
@@ -4,3 +4,4 @@ DOMAIN="domain"
 for username in `ls -1 /home/ | grep -v majorsza | grep -v pingvin | grep -v samba | grep -v BACKUP`; do
     chown -R "$username:$DOMAIN\domain users" /home/$username
 done
+
