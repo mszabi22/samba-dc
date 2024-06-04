3a0cf822a072b0e3177bc9596077ac31b1bd724f
Major Szabolcs <majorsza@localhost.localdomain>
2024-06-04 10:11:22 +0200

    Update: home permissions

--- USER-MANAGEMENT/set-home-permission.sh
+++ USER-MANAGEMENT/set-home-permission.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 DOMAIN="domain"
 
-#for username in `ls -1 /home/ | grep -v majorsza | grep -v pingvin | grep -v samba | grep -v BACKUP`; do
 for username in `samba-tool user list`; do
+if [ ! -d /home/$username ]; then
+  mkdir -p /home/$username;
+fi
 echo    chown -R "$username:$DOMAIN\domain users" /home/$username
 done
 
