3dc151eee303f1936e7fe6b8edceaa2d0bee50c6
Major Szabolcs <majorsza@localhost.localdomain>
2024-06-04 10:40:17 +0200

    Set user quota

--- USER-MANAGEMENT/set-user-quota.sh
+++ USER-MANAGEMENT/set-user-quota.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 QUOTA="0"
 
+echo "Set quota: /etc/zentyal/samba.conf"
+
 setquota -u $1 $QUOTA $QUOTA 0 0 /home
-setquota -u $1 $QUOTA $QUOTA 0 0 /dev/mapper/ubuntu--vg-ubuntu--lv
 
 echo "/home: "
 sudo repquota -vs /home  | grep $1
-echo ""
-echo "/dev/mapper/ubuntu--vg-ubuntu--lv: "
-sudo repquota -vs /dev/mapper/ubuntu--vg-ubuntu--lv  | grep $1
 echo ""
