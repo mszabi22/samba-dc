cc04400ecf83ef6a3af40416496355640378a888
Major Szabolcs <majorsza@localhost.localdomain>
2024-06-04 10:17:46 +0200

    Update: home quota

--- USER-MANAGEMENT/set-home-quota.sh
+++ USER-MANAGEMENT/set-home-quota.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 QUOTA="2G"
+
+#for username in `samba-tool user list`; do
 for username in `ls -1 /home/ | grep -v majorsza | grep -v pingvin | grep -v samba | grep -v BACKUP`; do
     setquota -u $username $QUOTA $QUOTA 0 0 /home
-    quota -vs $username
 done
+repquota -vs /home
